59118788b3cc4599eec8e2def2d799457df10cfa
video-yt <videoyt823@gmail.com>
2025-03-24 12:45:16 +0530

    Add files via upload

--- pair.js
+++ pair.js
@@ -77,13 +77,13 @@ var randomItem = selectRandomItem(items);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
                         let md = "DINU-MD=" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
-                        let desc = `*𝙳𝚘𝚗𝚝 𝚜𝚑𝚊𝚛𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚍𝚎 𝚠𝚒𝚝𝚑 𝚊𝚗𝚢𝚘𝚗𝚎!! 𝚄𝚜𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚍𝚎 𝚝𝚘 𝚌𝚛𝚎𝚊𝚝𝚎 QUEEN ANJU MD 𝚆𝚑𝚊𝚝𝚜𝚊𝚙𝚙 𝚄𝚜𝚎𝚛 𝚋𝚘𝚝.*\n\n ◦ *Github:* https://github.com/Mrrashmika/Queen_Anju-MD`; 
+                        let desc = `*𝙳𝚘𝚗𝚝 𝚜𝚑𝚊𝚛𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚍𝚎 𝚠𝚒𝚝𝚑 𝚊𝚗𝚢𝚘𝚗𝚎!! 𝚄𝚜𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚍𝚎 𝚝𝚘 𝚌𝚛𝚎𝚊𝚝𝚎 QUEEN DINU MD 𝚆𝚑𝚊𝚝𝚜𝚊𝚙𝚙 𝚄𝚜𝚎𝚛 𝚋𝚘𝚝.*\n\n ◦ *Github:* https://github.com/Dinuob/Queen_DNU-MD`; 
                         await sock.sendMessage(sock.user.id, {
 text: desc,
 contextInfo: {
 externalAdReply: {
 title: "QUEEN DINU MD",
-thumbnailUrl: "https://telegra.ph/file/adc46970456c26cad0c15.jpg",
+thumbnailUrl: "https://telegra.ph/QUEEN-DINU-MD-03-24.jpg",
 sourceUrl: "https://whatsapp.com/channel/0029Vaj5XmgFXUubAjlU5642",
 mediaType: 1,
 renderLargerThumbnail: true
@@ -99,7 +99,7 @@ text: desc,
 contextInfo: {
 externalAdReply: {
 title: "QUEEN DINU MD",
-thumbnailUrl: "https://telegra.ph/file/adc46970456c26cad0c15.jpg",
+thumbnailUrl: "https://telegra.ph/QUEEN-DINU-MD-03-24.jpg",
 sourceUrl: "https://whatsapp.com/channel/0029Vaj5XmgFXUubAjlU5642",
 mediaType: 2,
 renderLargerThumbnail: true,
